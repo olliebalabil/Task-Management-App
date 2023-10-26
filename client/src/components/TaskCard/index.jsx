4d2e23980a183f44abf47dbd64b27d101f8953d5
olliebalabil <oliverbalabil@gmail.com>
2023-10-26 19:48:59 +0100

    remove only for certain users

--- client/src/components/TaskCard/index.jsx
+++ client/src/components/TaskCard/index.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from "react"
 import "./TaskCard.css"
+import {useAuth} from "../../contexts"
 
 export default function TaskCard({id, name,notes, username, tasks, setTasks}) {
-
+const {user} = useAuth()
   const handleRemove = (e) => {
     const removeTask = async () => {
       const options = {
@@ -11,7 +12,11 @@ export default function TaskCard({id, name,notes, username, tasks, setTasks}) {
       const response = await fetch(`http://localhost:3000/tasks/${id}`, options)
       setTasks(tasks.filter(el=>el.id!==id))
     }
-    removeTask()
+    if (user === username || !username) {
+      removeTask()
+    } else (
+      alert("You can only delete tasks that you've created and that have anonymous creators.")
+    )
   }
   console.log(username)
   return(
@@ -20,6 +25,7 @@ export default function TaskCard({id, name,notes, username, tasks, setTasks}) {
      <h2>{name}</h2>
      <p>{notes}</p>
      {username && <p>Author: {username}</p>}
+     {!username && <p>Author: Anonymous</p>}
     </div>
     <div className="cardBtns">
     <input type="checkbox"/>
