73e0bdf467f8c55b00a5db65bf1aaf0ac533b10c
olliebalabil <oliverbalabil@gmail.com>
2023-10-26 17:11:59 +0100

    some changes to registering

--- client/src/pages/RegisterPage/index.jsx
+++ client/src/pages/RegisterPage/index.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react"
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 
 export default function RegisterPage() {
   const [textInput, setTextInput] = useState("")
   const [passwordInput, setPasswordInput] = useState("")
+  const [message, setMessage] = useState("")
+  const navigate = useNavigate()
+
 
   const handleTextInput = (e) => {
     setTextInput(e.target.value)
@@ -16,23 +19,43 @@ export default function RegisterPage() {
   const handleSubmit = (e) => {
     e.preventDefault()
     const register = async () => {
-      const options = {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          username: textInput,
-          password: passwordInput
-        })
+      try {
+        const options = {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: textInput,
+            password: passwordInput
+          })
+        }
+        const response = await fetch("http://localhost:3000/users/register", options)
+        const data = await response.json()
+        setTextInput("")
+        setPasswordInput("")
+        navigate("/login")
+      } catch (err) {
+        console.error(err.message)
+        setTextInput("")
+        setPasswordInput("")
+        setMessage("Register unsuccessful. Try again.")
+        setTimeout(()=>{
+          setMessage("")
+        },5000)
       }
-      const response = await fetch("http://localhost:3000/users/register", options)
-      const data = await response.json()
+
     }
-    register()
-setTextInput("")
-setPasswordInput("")
+    if (textInput && passwordInput) {
+      register()
+    } else {
+      setMessage("Register unsuccessful. Try again.")
+        setTimeout(()=>{
+          setMessage("")
+        },5000)
+    }
+
   }
   return (
     <>
@@ -45,6 +68,7 @@ setPasswordInput("")
         <input type="password" placeholder="Enter password here..." onChange={handlePasswordInput} value={passwordInput}></input>
         <input type="submit" />
       </form>
+      <p>{message}</p>
       <NavLink to="/login">Already a user? Login here</NavLink>
 
     </>
